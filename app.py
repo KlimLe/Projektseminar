4da6b6a48f13eef1861a6b716bf561a55acb892d
Fahdel <Fahdel.Kondodji@fau.de>
2025-06-19 18:48:41 +0200

    homepage.html in registration.html umbenannt; da index.html schon als Startseite dient

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
-def homepage():
-    return render_template('homepage.html')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
