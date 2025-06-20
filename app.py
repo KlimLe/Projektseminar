f6a70ae278552f373accad5b396aca2a60185877
Fahdel <Fahdel.Kondodji@fau.de>
2025-06-20 08:59:31 +0200

    Anpassungen am Registrierungsfeature

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('registration.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
